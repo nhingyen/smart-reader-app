a63c6737ef67feea94c753e3538cbceb9c9ab139
nhingyen <yennhing.work@gmail.com>
2025-10-09 22:21:07 +0700

    refactor(ui): replace TopCard with ListCard in CategoryDetailScreen

---
+++ lib/widgets/list_card.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:smart_reader/theme/app_colors.dart';
+
+class ListCard extends StatelessWidget {
+  final String imgUrl;
+  final String title;
+  final String author;
+  final String description;
+
+  const ListCard({
+    super.key,
+    required this.imgUrl,
+    required this.title,
+    required this.author,
+    required this.description,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white,
+      margin: const EdgeInsets.only(bottom: 12),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+        side: const BorderSide(color: Colors.black, width: 0.1), // viền đen
+      ),
+      elevation: 3,
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Cột 1: Ảnh
+            Container(
+              width: 90,
+              height: 120,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(6),
+                image: DecorationImage(
+                  image: NetworkImage(imgUrl),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+
+            const SizedBox(width: 12),
+
+            // Cột 2: Title + Author + Rating
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Text(
+                    author,
+                    style: const TextStyle(
+                      fontSize: 13,
+                      color: AppColors.primary,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Text(
+                    description,
+                    style: const TextStyle(
+                      fontSize: 12,
+                      color: Colors.black54,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    maxLines: 3,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
